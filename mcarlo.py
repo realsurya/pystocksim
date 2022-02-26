c62087564e5497ee907f1c883b3c549f92a28273
Surya Mani <smanikha@purdue.edu>
2022-02-25 23:43:07 -0500

    Starting Point
    Adding a starting point

---
+++ mcarlo.py
@@ -0,0 +1,27 @@
+#---------------------------------------#
+#               :PROJECT:               #
+#    ~Monte-Carlo Sim. for Equities~    #
+#              ~Pequities~              #
+#           ___           ___           #
+#          /\__\         /\  \          #
+#         /:/ _/_       |::\  \         #
+#        /:/ /\  \      |:|:\  \        #
+#       /:/ /::\  \   __|:|\:\  \       #
+#      /:/_/:/\:\__\ /::::|_\:\__\      #
+#      \:\/:/ /:/  / \:\~~\  \/__/      #
+#       \::/ /:/  /   \:\  \            #
+#        \/_/:/  /     \:\  \           #
+#          /:/  /       \:\__\          #
+#          \/__/         \/__/          #
+#                                       #
+#           Surya Manikhandan           #
+#             Feb. 25, 2022             #
+#    Aerospace Eng. Student @ Purdue    #
+#                                       #
+#        [E]:smanikha@purdue.edu        #
+#  [In]:linkedin.com/in/aerospacesurya  #
+#      [Git]: github.com/realsurya      #
+#---------------------------------------#
+
+
+
